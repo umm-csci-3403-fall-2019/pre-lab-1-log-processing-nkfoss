b906ca4fea035ef18a8578f87dd2dfb32112bef8
nkfoss <nathankfoss@gmail.com>
2019-09-05 09:45:06 -0500

    Modify regex.sh to parse r1 and add r1_output

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,4 +1,5 @@
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' r0_input.txt > r0_output.txt
 
-sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' r1_input.txt > r1_output.txt
+awk 'match($0, / \w\w ([A-Z]+[a-z]*).* (\w+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' r1_input.txt > r1_output.txt
+ 
 
